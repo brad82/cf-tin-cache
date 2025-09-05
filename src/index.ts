eeace6c9ccbb40c5e19b5dca61324bc4ec1cc4b6
Brad Morris <brad@bradleymorris.co.uk>
2025-09-04 22:09:55 -0600

    Log verb

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import { UCTinCanStatementRequest } from './types';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		//const path = '/ucTinCan/statements';
 
 		let verbs = JSON.parse((await env.TIN_CACHE.get('TC_VERB_WHITELIST')) ?? '[]');
 		const body = (await request.json()) as UCTinCanStatementRequest;
 		
+		console.log("VERB", body.verb.id)
+	
 		// Allow whitelisted verbs to continue straight to the server.
 		if (verbs.includes(body.verb.id)) {
 			return await fetch(request);
